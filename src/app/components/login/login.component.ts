e8d5c70d5a87d7ab520351ec135f22ccf366bd2d
Mauricio M <maury__25@hotmail.com>
2021-10-19 11:24:09 -0300

    update: setEnv

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
       recaptcha: ['', Validators.required]
     })
 
-    console.log("environment: ", environment.endpoint)
+    console.log("environment: ", environment)
     this.date = new Date()
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
+/* tslint:disable */
+// @ts-nocheck
+require('dotenv').config();
+
 export const environment = {
     endpoint: `"${process.env.endpoint}"`,
     production: true,
     secret_key: `"${process.env.secret_key }"`,
     siteKey: `"${process.env.siteKey }"`
   };
+
+  
+/* tslint:enable */
